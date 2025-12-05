c98068d889050ead4bcb40a72f337755aa7330ac
Thenujana <chenarathenujana@gmail.com>
2025-12-06 00:03:21 +0530

    database migrations-created the tables

--- database/migrations/2025_12_05_180302_create_employee_images_table.php
+++ database/migrations/2025_12_05_180302_create_employee_images_table.php
@@ -6,20 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+    
     public function up(): void
     {
         Schema::create('employee_images', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('employee_id')->constrained()->cascadeOnDelete();
+            $table->string('filename');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+   
     public function down(): void
     {
         Schema::dropIfExists('employee_images');
